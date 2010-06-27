9006eeec3656795269c05ab850bf56d00492d45b
Alex <zc00gii@gmail.com>
2010-06-27 03:19:47 -0400

    really deleting (start-sdl) now

--- simple-sdl-init.lisp
+++ simple-sdl-init.lisp
@@ -1,36 +1,5 @@
 (in-package :how)
 
-(defun start-sdl ()
-  (sdl:with-init (sdl:sdl-init-video )
-    (sdl:window *game-frame-size-x* *game-frame-size-y*)
-
-    (setf (sdl:frame-rate) 200)
-    (let ((house (load-image "house_64x64.bmp"))
-          (dude (make-instance 'how.characters::actor
-                               :surface (load-image "stickfigure_48x48.bmp")
-                               :name "Igoru"))
-	  (dude-position-x 0)
-	  (dude-position-y 0))
-
-      (sdl:with-events ()
-        (:quit-event ()
-                     (sdl:save-image sdl:*default-display*
-                                     (merge-pathnames #P"last-surface.bmp" +root-directory+))
-                     t)
-        (:key-down-event ()
-                         (sdl:push-quit-event))
-        (:mouse-motion-event (:x x :y y)
-                             (setf dude-position-x x)
-			     (setf dude-position-y y))
-        (:idle ()
-	       (sdl:clear-display sdl:*green*)
-	       (sdl:draw-surface-at-* house 100 100)
-	       (sdl:draw-surface-at-* (how.characters::surface dude)
-				      dude-position-x dude-position-y)
-	       (how.health::draw-health-at* (make-instance 'how.health::health :current 14 :maximum 20)
-					    :y 0 :x (- *game-frame-size-x* (* 10 how.health::*heart-image-side-length*)))
-               (sdl:update-display))))))
-
 (defun start-how ()
   (sdl:with-init (sdl:sdl-init-video )
     (sdl:window *game-frame-size-x* *game-frame-size-y*)
