16e481228389b9432da2280091a05426eea5356d
Alex <zc00gii@gmail.com>
2010-06-27 02:12:17 -0400

    fixed the functions

--- simple-sdl-init.lisp
+++ simple-sdl-init.lisp
@@ -8,7 +8,10 @@
     (let ((house (load-image "house_64x64.bmp"))
           (dude (make-instance 'how.characters::actor
                                :surface (load-image "stickfigure_48x48.bmp")
-                               :name "Igoru")))
+                               :name "Igoru"))
+	  (dude-position-x 0)
+	  (dude-position-y 0))
+
       (sdl:with-events ()
         (:quit-event ()
                      (sdl:save-image sdl:*default-display*
@@ -17,12 +20,15 @@
         (:key-down-event ()
                          (sdl:push-quit-event))
         (:mouse-motion-event (:x x :y y)
-                             (sdl:clear-display sdl:*green*)
-                             (sdl:draw-surface-at-* house 100 100)
-                             (sdl:draw-surface-at-* (how.characters::surface dude)
-                                                    x y)
-                             (how.health::draw-health-at* (make-instance 'how.health::health :current 14 :maximum 20) :y 0 :x (- *game-frame-size-x* (* 10 how.health::*heart-image-side-length*))))
+                             (setf dude-position-x x)
+			     (setf dude-position-y y))
         (:idle ()
+	       (sdl:clear-display sdl:*green*)
+	       (sdl:draw-surface-at-* house 100 100)
+	       (sdl:draw-surface-at-* (how.characters::surface dude)
+				      dude-position-x dude-position-y)
+	       (how.health::draw-health-at* (make-instance 'how.health::health :current 14 :maximum 20)
+					    :y 0 :x (- *game-frame-size-x* (* 10 how.health::*heart-image-side-length*)))
                (sdl:update-display))))))
 
 (defun start-sdl-keys ()
@@ -49,7 +55,7 @@
 	       (sdl:draw-surface-at-* house 100 100)
 	       (sdl:draw-surface-at-* (how.characters::surface dude)
 				      dude-position-x dude-position-y)
+	       (how.health::draw-health-at* (make-instance 'how.health::health :current 14 :maximum 20)
+					    :y 0 :x (- *game-frame-size-x* (* 10 how.health::*heart-image-side-length*)))
 	       (sdl:update-display))))))
-
-
 ;;; END
