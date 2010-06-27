5dcf38c91f10afb6b86ce495d1737ac9b340e54d
Alex <zc00gii@gmail.com>
2010-06-27 01:47:39 -0400

    made a funny working (start-sdl-keys) function

--- simple-sdl-init.lisp
+++ simple-sdl-init.lisp
@@ -29,4 +29,30 @@
         (:idle ()
                (sdl:update-display))))))
 
+(defun start-sdl-keys ()
+  (sdl:with-init (sdl:sdl-init-video )
+    (sdl:window *game-frame-size-x* *game-frame-size-y*)
+
+    (setf (sdl:frame-rate) 200)
+    (let ((house (load-image "house_64x64.bmp"))
+	  (dude (make-instance 'how.characters::actor
+			       :surface (load-image "stickfigure_64x64.bmp")
+			       :name "Igoru"))
+	  (dude-position-x 0)
+	  (dude-position-y 0))
+      (sdl:with-events ()
+	(:quit-event () t)
+	(:key-down-event (:key key)
+			 (when (sdl:key= key :sdl-key-q) (sdl:push-quit-event))
+			 (when (sdl:key= key :sdl-key-up) (setf dude-position-y (- dude-position-y 1)))
+			 (when (sdl:key= key :sdl-key-down) (setf dude-position-y (+ dude-position-y 1)))
+			 (when (sdl:key= key :sdl-key-left) (setf dude-position-x (- dude-position-x 1)))
+			 (when (sdl:key= key :sdl-key-right) (setf dude-position-x (+ dude-position-x 1))))
+	(:idle ()
+	       (sdl:clear-display sdl:*green*)
+	       (sdl:draw-surface-at-* house 100 100)
+	       (sdl:draw-surface-at-* (how.characters::surface dude)
+				      dude-position-x dude-position-y)
+	       (sdl:update-display))))))
+
 ;;; END
