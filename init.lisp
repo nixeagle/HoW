13bc5bf6378bdd4fb0278673061e073eb58678db
Alex <zc00gii@gmail.com>
2010-06-27 07:14:25 -0400

    hopefully last commit for a while -___-

--- init.lisp
+++ init.lisp
@@ -27,13 +27,12 @@ solves most issues.")
   (loop for cell-y from 0 to (- width y) by y
                             append (loop for cell-x from 0 to (- height x) by x
                                          collect (list cell-x cell-y x y))))
-(defun load-sprite-set (sprite-sheet x y)
+(defun load-sprite-sheet (sprite-sheet x y)
   "Set the cells for a sprite sheet at `sdl:cells' using `generate-cells'"
-  (setf (sdl:cells sprite-sheet) (generate-cells x y (sdl:width sprite-set) (sdl:height sprite-sheet))))
+  (setf (sdl:cells sprite-sheet) (generate-cells x y (sdl:width sprite-sheet) (sdl:height sprite-sheet))))
 
-(defun set-sprite-set (sprite-sheet x y cell)
+(defun set-sprite-sheet (sprite-sheet x y cell)
   "draw surface for a sprite sheet"
-  (sdl:draw-surface-at sprite-sheet (apply #'vector (list x y)) :cell cell)
-  (sdl:load-image (merge-pathnames name directory) :color-key (sdl:color :r 255 :g 0 :b 255)))
+  (sdl:draw-surface-at sprite-sheet (apply #'vector (list x y)) :cell cell))
 
 ;;; END
