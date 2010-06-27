b1629dd6e1a9a20c553a32ed2a320d2cb9e489aa
Alex <zc00gii@gmail.com>
2010-06-27 19:25:25 -0400

    added key-events

--- simple-sdl-init.lisp
+++ simple-sdl-init.lisp
@@ -1,39 +1,5 @@
 (in-package :how)
 
-(defgeneric handle-key-down-event
-    (key state &key &allow-other-keys)
-  (:documentation "Handle key down presses."))
-
-(defmethod handle-key-down-event :around
-    ((key t) (state t) &key (surface sdl:*default-surface*)
-     (display sdl:*default-display*))
-  "Bind display and surface before calling onwards."
-  (let ((sdl:*default-display* display)
-        (sdl:*default-surface* surface))
-    (call-next-method)))
-
-(defgeneric optional-arg-form (input &key gensym-name))
-(defmethod optional-arg-form ((input (eql nil)) &key (gensym-name "G"))
-  `(,(gensym gensym-name) t))
-(defmethod optional-arg-form ((input symbol) &key (gensym-name "G"))
-  `(,(gensym gensym-name) (eql ,input)))
-(defmethod optional-arg-form ((input list) &key (gensym-name "G"))
-  (declare (ignore gensym-name))
-  input)
-
-(defmacro define-key-down-event ((key &optional state &rest keys) &body body)
-  `(defmethod handle-key-down-event
-       (,(optional-arg-form key :gensym-name "KEY-")
-        ,(optional-arg-form state :gensym-name "STATE-") &key ,@keys)
-     ,@body))
-
-(define-key-down-event (:sdl-key-escape)
-  "Leave the game!"
-  (sdl:push-quit-event))
-(define-key-down-event ((key t) (state t))
-  "If we don't handle the key, say something!"
-  (format t "No key-down event! Key: ~S State: ~S~%" key state))
-
 (let (fire)
   (defmethod handle-key-down-event :before ((key t) (state (eql :fire)) &key)
     (or fire (progn (setq fire (how.sprite::load-sprite-sheet (load-image "ani2.bmp") :x 64 :y 64)) fire)))
@@ -99,15 +65,18 @@
 			 (when (sdl:key= key :sdl-key-down) (setf dude-position-y (+ dude-position-y 1)))
 			 (when (sdl:key= key :sdl-key-left) (setf dude-position-x (- dude-position-x 1)))
 			 (when (sdl:key= key :sdl-key-right) (setf dude-position-x (+ dude-position-x 1)))
-                         (sdl:clear-display sdl:*green*)
                          (sdl:draw-surface-at-* house 100 100)
                          (sdl:draw-surface-at-* (how.characters::surface dude)
                                                 dude-position-x dude-position-y)
-                         (handle-key-down-event key :fire)
-
-                         (how.health::draw-health-at* (make-instance 'how.health::health :current 14 :maximum 20)
-                                                      :y 0 :x (- *game-frame-size-x* (* 10 how.health::*heart-image-side-length*))))
+			 (sdl:clear-display sdl:*green*)
+			 (sdl:draw-surface-at-* house 100 100)
+			 (sdl:draw-surface-at-* (how.characters::surface dude)
+						dude-position-x dude-position-y)
+			 (handle-key-down-event key :fire)
+			 (how.health::draw-health-at* (make-instance 'how.health::health :current 14 :maximum 20)
+						      :y 0 :x (- *game-frame-size-x* (* 10 how.health::*heart-image-side-length*)))
+			 (how.health::draw-health-at* (make-instance 'how.health::health :current 14 :maximum 20)
+						      :y 0 :x (- *game-frame-size-x* (* 10 how.health::*heart-image-side-length*))))
 	(:idle ()
-
 	       (sdl:update-display))))))
 ;;; END
---
+++ src/key-events.lisp
@@ -0,0 +1,35 @@
+(in-package :how)
+
+(defgeneric handle-key-down-event
+    (key state &key &allow-other-keys)
+  (:documentation "Handle key down presses."))
+
+(defmethod handle-key-down-event :around
+    ((key t) (state t) &key (surface sdl:*default-surface*)
+     (display sdl:*default-display*))
+  "Bind display and surface before calling onwards."
+  (let ((sdl:*default-display* display)
+        (sdl:*default-surface* surface))
+    (call-next-method)))
+
+(defgeneric optional-arg-form (input &key gensym-name))
+(defmethod optional-arg-form ((input (eql nil)) &key (gensym-name "G"))
+  `(,(gensym gensym-name) t))
+(defmethod optional-arg-form ((input symbol) &key (gensym-name "G"))
+  `(,(gensym gensym-name) (eql ,input)))
+(defmethod optional-arg-form ((input list) &key (gensym-name "G"))
+  (declare (ignore gensym-name))
+  input)
+
+(defmacro define-key-down-event ((key &optional state &rest keys) &body body)
+  `(defmethod handle-key-down-event
+       (,(optional-arg-form key :gensym-name "KEY-")
+        ,(optional-arg-form state :gensym-name "STATE-") &key ,@keys)
+     ,@body))
+
+(define-key-down-event (:sdl-key-escape)
+  "Leave the game!"
+  (sdl:push-quit-event))
+(define-key-down-event ((key t) (state t))
+  "If we don't handle the key, say something!"
+  (format t "No key-down event! Key: ~S State: ~S~%" key state))
